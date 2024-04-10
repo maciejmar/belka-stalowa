86f89448e289a4af10eb8cfd515743906ef3dc68
maciejmar <memgos@gmail.com>
2024-04-10 06:17:23 +0200

    pagination added

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -10,14 +10,29 @@ import { DataFormService } from '../data-form.service';
 })
 export class ResultsComponent implements OnInit {
 results:Results[]=[]
-  constructor(private dataFromForm: DataFormService) { }
+currentPage: number = 1;
+itemsPerPage: number = 10;
+totalPages: number=0;
+  constructor(private dataFromForm: DataFormService) { 
+    this.totalPages = Math.ceil(this.results.length / this.itemsPerPage);
+  }
 
   ngOnInit(): void {
      this.getAllResults();
   }
 
   getAllResults(){
-    this.dataFromForm.getResults().subscribe(data =>this.results = data);
+    this.dataFromForm.getResults().subscribe(data =>this.results = data.reverse());
+  }
+
+  getItemsForCurrentPage(): string {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.results.toString().slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  goToPage(page: number): void {
+    if (page < 1 || page > this.totalPages) return;
+    this.currentPage = page;
   }
 
 }
