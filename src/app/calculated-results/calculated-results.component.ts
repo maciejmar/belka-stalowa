18b885c25f4321875ca9c6828b46ded0803e71b1
maciejmar <memgos@gmail.com>
2024-04-13 18:31:29 +0200

    extract method added

--- src/app/calculated-results/calculated-results.component.ts
+++ src/app/calculated-results/calculated-results.component.ts
@@ -64,6 +64,15 @@ export class CalculatedResultsComponent implements OnInit {
        
     return this.Minim;
   }
+  extract(): string[]{
+   let res = this.results[this.results.length - 1];
+   let resTemp = res.toString().split(',');
+   let resTemp2 = resTemp[6];
+   let resTemp3 = resTemp[7];
+   let ret = [];
+   ret.push(resTemp2,resTemp3);
+   return ret
+  }
 
   extractWmin(): number | null {
     // Assuming the string is in `results.name` and the pattern is "Wmin:value"
